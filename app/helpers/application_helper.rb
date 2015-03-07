faf243bf7155af77417f49d4777b574d3d18b0f9
zhiren92 <zillah_c@hotmail.com>
2015-03-07 13:48:48 -0800

    add rescue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,28 @@
 module ApplicationHelper
   # after getting a datetime obj this will return an arr with the range 
-  def get_range(time_obj)
-    range_beg = time_obj.start_time
-    range_end = time_obj.end_time
-    user = time_obj.user.name
 
-    range = [range_beg, range_end, user]
+  def get_range(time_obj)
+    begin
+      range_beg = time_obj.start_time
+      range_end = time_obj.end_time
+      user = time_obj.user.name
+
+      range = [range_beg, range_end, user]
+    rescue
+    end
   end
 
 # you can get the arr with multiple time ranges after inputting the total available_time arr
+# [[beg1, end1, user]]
 
   def get_range_arr(time_obj_arr)
-    if time_obj_arr
+    begin
       range_arr=[]
       for i in 0...time_obj_arr.length
         range_arr.push(get_range(time_obj_arr[i]))
       end
       range_arr
+    rescue
     end
   end
 
@@ -25,15 +31,18 @@ module ApplicationHelper
     # if start_time1 is >= start_time2 and < end_time2 
     # or end_time1 is < end_time2 and > start_time2
     # then overlaps
-    overlap_start = [range1[0], range2[0]].max
-    overlap_end = [range1[1], range2[1]].min
+    begin
+      overlap_start = [range1[0], range2[0]].max
+      overlap_end = [range1[1], range2[1]].min
 
-    if (range1[0] >= range2[0] && range1[0] < range2[1]) || (range1[1] < range2[1] && range1[1] > range2[0])
-      return [overlap_start, overlap_end, range1[2], range2[2]]
-    elsif (range1[0]>range2[0]&&range1[1]<range1[1]) 
-      return [range1[0], range1[1],range1[2], range2[2]]
-    else
-      return [range2[0], range2[1],range1[2], range2[2]]
+      if (range1[0] >= range2[0] && range1[0] < range2[1]) || (range1[1] < range2[1] && range1[1] > range2[0])
+        return [overlap_start, overlap_end, range1[2], range2[2]]
+      elsif (range1[0]>range2[0]&&range1[1]<range1[1]) 
+        return [range1[0], range1[1],range1[2], range2[2]]
+      else
+        return [range2[0], range2[1],range1[2], range2[2]]
+      end
+    rescue
     end
   end
 
@@ -42,13 +51,17 @@ module ApplicationHelper
 # returns an array of overlapping times 
 
   def available_time_overlap(arr1, arr2)
-   overlap_arr = []
-    for i in 0...arr1.length
-      for j in 0...arr2.length
-        overlap_arr.push(range_overlap(arr1[i], arr2[j]))
+    begin
+     overlap_arr = []
+      for i in 0...arr1.length
+        for j in 0...arr2.length
+          overlap_arr.push(range_overlap(arr1[i], arr2[j]))
+        end
       end
+      overlap_arr
+    
+    rescue
     end
-    overlap_arr
   end
 
   
