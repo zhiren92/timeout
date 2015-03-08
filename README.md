0be89d3635ad5cee967c54c304a011ae0b784b65
zhiren92 <zillah_c@hotmail.com>
2015-03-08 16:43:07 -0700

    add details on signin

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 <h1>TimeOut</h1>
 
-[![Circle CI](https://circleci.com/gh/zhiren92/timeout.svg?style=svg)](https://circleci.com/gh/zhiren92/timeout)
-
 [![Code Climate](https://codeclimate.com/github/zhiren92/timeout/badges/gpa.svg)](https://codeclimate.com/github/zhiren92/timeout)
+
+Timeout is an application that allows you to connect and keep in touch with your friends
+
+###Technology
+*Ruby on Rails
+*PostgreSQL
+*AngularJS
+*Bootstrap
+
+###Instructions
+<ul>
+<li>Add your friends within the app</li>
+<li>Set your availability </li>
+<li>Watch the app match your friends availabilities with you </li>
+<ul>
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
 
+  has_secure_password
+  attr_reader :password
+  
+  
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
+
   has_many :friendship_requests
   has_many :pendingfriends, through: :friendship_requests
   has_many :request_made, class_name: FriendshipRequest
@@ -57,12 +65,8 @@ class User < ActiveRecord::Base
     User.all - self.requesters - self.requestees-self.friends-User.where(id:self.id)
   end 
 
-  
-  has_secure_password
-  attr_reader :password
 
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: {case_sensitive: false}, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
 
   def as_json(option={})
     super(:except => [:password_digest, :created_at, :updated_at])
